922f80128126a52702244b329222a79fe51f2e3a
jundong <1966273343@qq.com>
2024-09-02 10:02:04 +0800

    add employee-detail

--- catalog/urls.py
+++ catalog/urls.py
@@ -4,7 +4,7 @@ from . import views
 urlpatterns = [
     path('', views.index, name='index'),
     path('departments/', views.DepartmentListView.as_view(), name='departments'),
-    path('department/<int:pk>',views.DepartmentDetailView.as_view(), name='department-detail')
-    path('department/employee/<int:pk>', views.EmployeeDetailView.as_view(), name='employee-detail')
+    path('department/<int:pk>',views.DepartmentDetailView.as_view(), name='department-detail'),
+    path('department/employee/<uuid:pk>', views.EmployeeDetailView.as_view(), name='employee-detail'),
 ]
 
